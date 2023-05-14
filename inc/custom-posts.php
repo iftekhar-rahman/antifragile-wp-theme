4614c02dc0064168f2cd47985566d08dc9377ae4
Iftekhar Rahman <iftekharmail1@gmail.com>
2023-05-14 19:40:59 +0600

    Concept custom post added

--- inc/custom-posts.php
+++ inc/custom-posts.php
@@ -24,4 +24,28 @@ function antifragile_services() {
 
 add_action( 'init', 'antifragile_services' );
 
+/* Services Custom Post */
+function antifragile_concept() {
+	$labels = array(
+		'name'                  => _x( 'Concept', 'Post type general name', 'antifragile' ),
+		'singular_name'         => _x( 'Concept', 'Post type singular name', 'antifragile' ),
+		'menu_name'             => _x( 'Concept', 'Admin Menu text', 'antifragile' ),
+		'name_admin_bar'        => _x( 'Concept', 'Add New on Toolbar', 'antifragile' ),
+		'add_new'               => __( 'Add Concept', 'antifragile' ),
+		'add_new_item'          => __( 'Add New Concept', 'antifragile' ),
+	);
+	$args = array(
+		'labels'             => $labels,
+		'public'             => true,
+		'description'        => 'Concept custom post type.',
+		'rewrite'            => array( 'slug' => 'concept' ),
+		'menu_position'      => null,
+		'supports'           => array( 'title', 'thumbnail', 'excerpt' ),
+	);
+
+	register_post_type( 'concept', $args );
+}
+
+add_action( 'init', 'antifragile_concept' );
+
 
